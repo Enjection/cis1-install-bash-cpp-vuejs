fcf42adbe42bc51b42995bb7649681c7660a4c06
Innokentiy Mokin <enjection@ya.ru>
2019-06-14 17:21:55 +0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf cis1
+mkdir cis1
+
+git clone https://github.com/tomsksoft-llc/cis1-core-bash.git cis1
+git clone https://github.com/Enjection/cis1-install-bash-cpp-vuejs cis1/jobs/maintenance
+git clone https://github.com/tomsksoft-llc/cis1-webui-native-front-vuejs.git cis1/jobs/maintenance/build_cpp_websrv/cis1-webui-native-front-vuejs
+git clone https://github.com/tomsksoft-llc/cis1-webui-native-srv-cpp.git cis1/jobs/maintenance/build_vuejs_webui/cis1-webui-native-srv-cpp
