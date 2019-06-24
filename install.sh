26c4b2f055ffda52d1cdbd24e527f61be9e4eb31
Innokentiy Mokin <enjection@ya.ru>
2019-06-24 16:24:49 +0700

    add possibility to run job from installer

--- install.sh
+++ install.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-rm -rf cis1
-mkdir cis1
+read -p "Enter a directory: " -e dir
 
-export cis_base_dir=`pwd`/cis1
+dir=`realpath $dir`
 
-git clone https://github.com/tomsksoft-llc/cis1-core-bash.git cis1
-git clone https://github.com/Enjection/cis1-install-bash-cpp-vuejs cis1/jobs/maintenance
-git clone https://github.com/tomsksoft-llc/cis1-webui-native-front-vuejs.git cis1/jobs/maintenance/build_vuejs_webui/cis1-webui-native-front-vuejs
-git clone https://github.com/tomsksoft-llc/cis1-webui-native-srv-cpp.git cis1/jobs/maintenance/build_cpp_websrv/cis1-webui-native-srv-cpp
+rm -rf $dir
+mkdir $dir
 
-mkdir cis1/webui
+export cis_base_dir=$dir
 
-cp cis1/jobs/maintenance/build_cpp_websrv/cis1-webui-native-srv-cpp/example_config.ini cis1/webui/config.ini
+git clone https://github.com/tomsksoft-llc/cis1-core-bash.git $cis_base_dir
+git clone https://github.com/Enjection/cis1-install-bash-cpp-vuejs $cis_base_dir/jobs/maintenance
+git clone https://github.com/tomsksoft-llc/cis1-webui-native-front-vuejs.git $cis_base_dir/jobs/maintenance/build_vuejs_webui/cis1-webui-native-front-vuejs
+git clone https://github.com/tomsksoft-llc/cis1-webui-native-srv-cpp.git $cis_base_dir/jobs/maintenance/build_cpp_websrv/cis1-webui-native-srv-cpp
+
+mkdir $cis_base_dir/webui
+
+cp $cis_base_dir/jobs/maintenance/build_cpp_websrv/cis1-webui-native-srv-cpp/example_config.ini $cis_base_dir/webui/config.ini
 
 if [ "x$EDITOR" = "x" ]
 then
-    nano cis1/webui/config.ini
+    nano $cis_base_dir/webui/config.ini
 else
-    $EDITOR cis1/webui/config.ini
+    $EDITOR $cis_base_dir/webui/config.ini
 fi
+
+while true; do
+    read -p "Do you wish to compile and run webui?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+source run.sh
